83d4db51fc16844767e37319b41314c7b22124dd
Anmol Jande <jandanmol@gmail.com>
2019-01-11 17:37:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function updateObjectWithKeyAndValue(object, key, value)
  return Object.assign({},object,{[key]:value})
  
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object,key, value)
+{
+  object[key]=value;
+  return object;
+}
+
